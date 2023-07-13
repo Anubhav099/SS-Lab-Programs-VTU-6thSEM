b66c2fb98f7dcca17f76990e1c6843a9672423b9
Anubhav099 <ait.anubhav@gmail.com>
2023-07-14 01:35:45 +0530

    modified

--- 9/9.c
+++ 9/9.c
@@ -3,16 +3,17 @@
 
 int main()
 {
-    int ch,YN=1,i,l,f;
-    char F[10],s[25];
+    int ch, YN, i, f;
+    char frame[10],str[25];
+
     printf("\nEnter the no of empty frames: ");
     scanf("%d",&f);
-    printf("\nEnter the length of the string: ");
-    scanf("%d",&l);
+
     printf("\nEnter the string: ");
-    scanf("%s",s);
-    for(i=0;i<f;i++)
-        F[i]=-1;
+    scanf("%s",str);
+    int l = strlen(str);
+
+    for(i=0;i<f;i++)    frame[i]=-1;
 
     do
     {
@@ -20,39 +21,39 @@ int main()
         printf("\n1:FIFO\n2:LRU");
         printf("\nEnter your choice: ");
         scanf("%d",&ch);
-	printf("\nPAGE\t\tFRAMES\t\tFAULTS");
+
+        for(i=0;i<f;i++) frame[i]=-1;
+	    printf("\nPAGE\t\tFRAMES\t\tFAULTS");
         switch(ch)
         {
-            case 1: for(i=0;i<f;i++) F[i]=-1;
-                    FIFO(s,F,l,f);
-                    break;
+            case 1: FIFO(str,frame,l,f); break;
 
-            case 2: for(i=0;i<f;i++) F[i]=-1;
-                    LRU(s,F,l,f);
+            case 2: LRU(str,frame,l,f);
         }
         printf("\n\nPress 0 to Exit and 1 to continue : ");
         scanf("%d",&YN);
     } while(YN==1);
+
     return(0);
 }
 
-void FIFO(char s[],char F[],int l,int f)
+void FIFO(char str[],char frame[],int l,int f)
 {
     int i,j=0,k,flag=0,cnt=0;
     for(i=0;i<l;i++)
     {
         for(k=0;k<f;k++)
         {
-            if(F[k]==s[i])
+            if(frame[k]==str[i])
                 flag=1;
         }
 
         if(flag==0)
         {
-            printf("\n%c\t",s[i]);
-            F[j]=s[i];
+            printf("\n%c\t",str[i]);
+            frame[j]=str[i];
             j++;
-            for(k=0;k<f;k++) printf("\t%c",F[k]);
+            for(k=0;k<f;k++) printf("\t%c",frame[k]);
             printf("\tPage-fault%d",cnt);
             cnt++;
         }
@@ -60,51 +61,48 @@ void FIFO(char s[],char F[],int l,int f)
         else
         {
             flag=0;
-            printf("\n%c\t",s[i]);
-            for(k=0;k<f;k++) printf("\t%c",F[k]);
+            printf("\n%c\t",str[i]);
+            for(k=0;k<f;k++) printf("\t%c",frame[k]);
             printf("\tNo page-fault");
         }
         if(j==f) j=0;
     }
 }
 
-void LRU(char s[],char F[],int l,int f)
+void LRU(char str[],char frame[],int l,int f)
 {
     int i,j=0,k,m,flag=0,cnt=0,top=0;
     for(i=0;i<l;i++)
     {
         for(k=0;k<f;k++)
-            if(F[k]==s[i])
+            if(frame[k]==str[i])
             {
                 flag=1;
                 break;
             }
-        printf("\n%c\t",s[i]);
+
+        printf("\n%c\t",str[i]);
         if(j!=f && flag!=1)
         {
-            F[top]=s[i];
+            frame[top]=str[i];
             j++;
             if(j!=f) top++;
         }
-
         else
         {
             if(flag!=1)
             {
-                for(k=0;k<top;k++)
-                    F[k]=F[k+1];
-                F[top]=s[i];
+                for(k=0;k<top;k++)  frame[k]=frame[k+1];
+                frame[top]=str[i];
             }
-
-            if(flag==1)
+            else if(flag==1)
             {
-                for(m=k;m<top;m++)
-                    F[m]=F[m+1];
-                F[top]=s[i];
+                for(m=k;m<top;m++)  frame[m]=frame[m+1];
+                frame[top]=str[i];
             }
         }
 
-        for(k=0;k<f;k++) printf("\t%c",F[k]);
+        for(k=0;k<f;k++) printf("\t%c",frame[k]);
 
         if(flag==0)
         {
